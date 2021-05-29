6745bb77ef81cb71f330771879bd970366b53773
Teddy Sawon <marcoexico00@gmail.com>
2021-05-29 15:01:15 +0700

    default

--- resources/views/mahasiswa.blade.php
+++ resources/views/mahasiswa.blade.php
@@ -15,7 +15,13 @@
     </head>
 
     <body>
-
+        <?php
+        $output = "SELECT *
+            FROM mahasiswa
+            INNER JOIN matkul
+            ON mahasiswa.nbi = matkul.nbi
+            ORDER BY mahasiswa.nbi"
+        ?>
         <script src="/bootstrap/bootstrap-5.0.0-beta3-dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
     </body>
 </html>
